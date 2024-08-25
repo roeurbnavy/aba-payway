161b57ee3d282182ac3563fe51d3bae2996904e0
Navy <roeurbnavy@gmail.com>
2024-08-25 12:15:48 +0700

    test aba payway

--- imports/api/files/methods.js
+++ imports/api/files/methods.js
@@ -1,5 +1,6 @@
 import { Meteor } from "meteor/meteor";
-
+import Base64 from "crypto-js/enc-base64";
+import { HmacSHA512 } from "crypto-js";
 // // Collections
 import Files from "./files";
 Meteor.methods({
@@ -42,4 +43,82 @@ Meteor.methods({
       return null;
     }
   },
+  abaPayway(total) {
+    if (Meteor.isServer) {
+      console.log("total", total);
+      // Define constants
+      const ABA_PAYWAY_API_URL =
+        "https://checkout-sandbox.payway.com.kh/api/payment-gateway/v1/payments/purchase";
+      const ABA_PAYWAY_MERCHANT_ID = "ec438088";
+      // Handle form submission
+      console.log("called");
+      const items = Buffer.from(
+        JSON.stringify([
+          { name: "test1", quantity: "1", price: "10.00" },
+          { name: "test2", quantity: "1", price: "10.00" },
+        ])
+      ).toString("base64");
+      const req_time = Math.floor(Date.now() / 1000);
+      const transactionId = req_time;
+      const amount = total;
+      const firstName = "Makara";
+      const lastName = "Prom";
+      const phone = "015590085";
+      const email = "roeurbnaby@gmail.com";
+      const return_params = "Hello World!";
+      const type = "purchase";
+      const currency = "USD";
+      const payment_option = "abapay";
+      const shipping = "1.35";
+
+      const generate =
+        req_time +
+        ABA_PAYWAY_MERCHANT_ID +
+        transactionId +
+        amount +
+        items +
+        shipping +
+        firstName +
+        lastName +
+        email +
+        phone +
+        type +
+        payment_option +
+        currency +
+        return_params;
+      const hash = getHash(generate);
+
+      console.log("hash", hash);
+
+      return {
+        ABA_PAYWAY_API_URL,
+        hash,
+        transactionId,
+        amount,
+        firstName,
+        lastName,
+        phone,
+        email,
+        items,
+        return_params,
+        shipping,
+        currency,
+        type,
+        payment_option,
+        ABA_PAYWAY_MERCHANT_ID,
+        req_time,
+      };
+    }
+  },
 });
+
+// Function to generate HMAC hash
+function getHash(str) {
+  const ABA_PAYWAY_API_KEY = "965dd4e87cce272badd4a346181ce5d815da2ab3";
+  const hmac = HmacSHA512(str, ABA_PAYWAY_API_KEY);
+  return Base64.stringify(hmac);
+  // const hmac = createHmac("sha512", ABA_PAYWAY_API_KEY)
+  //   .update(str)
+  //   .digest("base64");
+  // return hmac;
+}
--- imports/ui/Home.vue
+++ imports/ui/Home.vue
@@ -1,91 +1,72 @@
 <script setup>
+import { nextTick, ref } from "vue";
+
 // Csllections
-import FilesCollection from '/imports/api/files/files'
-import { onMounted, ref } from 'vue'
+const amount = ref(0);
+const form = ref({
+  req_time: "",
+  transactionId: "",
+  amount: 0,
+  firstName: "",
+  lastName: "",
+  phone: "",
+  email: "",
+  return_params: "",
+  type: "",
+  currency: "USD",
+  payment_option: "",
+  shipping: "",
+});
+const checkout = () => {
+  if (amount.value == 0) return false;
 
-const file = ref('')
-const fileData = ref([])
-
-const uploadToServer = () => {
-  FilesCollection.insert({
-    file: file.value,
-    chunkSize: 'dynamic',
-    // onBeforeUpload(file) {
-    //   // Allow upload files under 1MB, and only in png/jpg/jpeg formats
-    //   if (file.size <= 2000 && /png|jpg|jpeg/i.test(file.extension)) {
-    //     return true
-    //   }
-    //   return 'Please upload image, with size equal or less than 2MB'
-    // },
-    onUploaded(err, fileObj) {
-      if (err) {
-        console.log('error', err)
-      } else {
-        file.value = ''
-        getFileUploads()
-      }
-    },
-  })
-}
-
-const handleChange = ($event) => {
-  const target = $event.target
-  if (target && target.files) {
-    file.value = target.files[0]
-    // uploadToServer(target.files[0])
-  }
-}
-const handleUpload = () => {
-  if (!file.value) {
-    alert('file required.')
-    return false
-  }
-  uploadToServer()
-}
-
-const getFileUploads = () => {
-  Meteor.call('findImageData', (err, res) => {
-    if (err) {
-      console.log('error', err)
-    } else {
-      console.log('res', res)
-      fileData.value = res
-    }
-  })
-}
-
-const removeFile = (id) => {
-  console.log('id', id)
-  FilesCollection.remove({ _id: id }, (err) => {
-    if (!err) {
-      getFileUploads()
-    } else {
-      console.log(err)
-    }
-  })
-}
-
-onMounted(() => {
-  getFileUploads()
-})
+  Meteor.call("abaPayway", amount.value, (err, res) => {
+    console.log(res);
+    // renderData.value = res;
+    form.value = res;
+    // form.value.amount = amount.value;
+    nextTick(() => {
+      document.getElementById("aba_merchant_request")?.submit();
+    });
+  });
+};
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold my-6">File Uploads</h1>
-  <ul>
-    <li v-for="file in fileData" :key="file._id">
-      <img :src="file.url" style="width: 125px; height: 125px" />
-      <button type="button" @click="removeFile(file._id)">X</button>
-    </li>
-  </ul>
-  <hr class="mb-2" />
-  <input type="file" @change="handleChange" />
+  <h1 class="text-3xl font-bold my-6">ABA Payway</h1>
+  <input class="border p-1 mr-1 rounded" v-model.number="amount" />
   <button
     class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-4 border border-blue-500 hover:border-transparent rounded"
     type="button"
-    @click="handleUpload"
-    v-if="file"
+    @click="checkout"
   >
-    Upload
+    Checkout
   </button>
+
+  <form
+    method="POST"
+    :action="form.ABA_PAYWAY_API_URL"
+    id="aba_merchant_request"
+  >
+    <input type="hidden" name="hash" :value="form.hash" />
+    <input type="hidden" name="tran_id" :value="form.transactionId" />
+    <input type="hidden" name="amount" :value="form.amount" />
+    <input type="hidden" name="firstname" :value="form.firstName" />
+    <input type="hidden" name="lastname" :value="form.lastName" />
+    <input type="hidden" name="phone" :value="form.phone" />
+    <input type="hidden" name="email" :value="form.email" />
+    <input type="hidden" name="items" :value="form.items" />
+    <input type="hidden" name="return_params" :value="form.return_params" />
+    <input type="hidden" name="shipping" :value="form.shipping" />
+    <input type="hidden" name="currency" :value="form.currency" />
+    <input type="hidden" name="type" :value="form.type" />
+    <input type="hidden" name="payment_option" :value="form.payment_option" />
+
+    <input
+      type="hidden"
+      name="merchant_id"
+      :value="form.ABA_PAYWAY_MERCHANT_ID"
+    />
+    <input type="hidden" name="req_time" :value="form.req_time" />
+  </form>
 </template>
