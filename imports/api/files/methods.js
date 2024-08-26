bf405cef73dbf11f98d634df22c9438c5120a62f
Navy <roeurbnavy@gmail.com>
2024-08-26 09:48:56 +0700

    remove google and aws key

--- imports/api/files/methods.js
+++ imports/api/files/methods.js
@@ -1,48 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import Base64 from "crypto-js/enc-base64";
 import { HmacSHA512 } from "crypto-js";
-// // Collections
-import Files from "./files";
 Meteor.methods({
-  findImageData: () => {
-    if (Meteor.isServer) {
-      try {
-        let images = Files.find(
-          {},
-          {
-            sort: { date: -1 },
-          }
-        ).fetch();
-
-        const url = Meteor.absoluteUrl();
-        images.forEach((o) => {
-          // o.url = Files.findOne({ _id: o._id }).link();
-          const fileRef = Files.collection.findOne({ _id: o._id });
-          o.url = Files.link(fileRef, "original", url);
-        });
-
-        return images;
-      } catch (error) {
-        console.log("error", error);
-      }
-    }
-  },
-  findImage: ({ _id }) => {
-    if (Meteor.isServer) {
-      let images = Files.find({ _id }).fetch();
-
-      if (images.length) {
-        let image = images[0];
-
-        // Get image url
-        image.url = Files.findOne({ _id: image._id }).url();
-
-        return image;
-      }
-
-      return null;
-    }
-  },
   abaPayway(total) {
     if (Meteor.isServer) {
       console.log("total", total);
